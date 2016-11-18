de8efb501f454690191d8a53fe356ca03282149c
cmabdullah <a.kium.khan@gmail.com>
2016-11-18 14:21:41 +0600

    log in part have develoved successfully

---
+++ copa_america/admin_panel.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel</title>
+</head>
+<body>
+
+<h1>this page is under construction</h1>
+
+</body>
+</html>
--- copa_america/log_in.php
+++ copa_america/log_in.php
@@ -20,16 +20,19 @@
 	</style>
 </head>
 <body>
+
+
+
 <?php
 session_start();
 if(isset($_POST['bttLogin'])){
 	require 'connect.php';
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	$result = mysqli_query($con, 'select * from login_info where username="'.$username.'" and password= "'.$password.'"');
+	$result = mysqli_query($con, 'select * from admin_data where username="'.$username.'" and password= "'.$password.'"');
 	if(mysqli_num_rows($result)==1){
 		$_SESSION['username'] = $username;
-		header('location: logout.php');// header location shift to logout page
+		header('location: admin_panel.php');// header location shift to logout page
 	}
 	else
 		echo "Account invalid";
@@ -38,6 +41,14 @@ if (isset($_GET['logout'])){
 	session_unregister('username');
 }
 ?>
+
+
+
+
+
+
+
+<!--
 <br>
 <div align="center"><img class="copa-logo" src="logo.png"></div>
 <br><br><br>
@@ -56,11 +67,37 @@ if (isset($_GET['logout'])){
 
 		<tr>
 		<td>&nbsp;</td>
+		<td><input type="submit"  value="Login"></td>
+	</tr>
+	</table>
+</form>
+</div>
+
+-->
+
+<br>
+<div align="center"><img class="copa-logo" src="logo.png"></div>
+<br><br>
+
+<div align="center"  class="main"> <br>
+<form method="post">
+	<table cellspacing="2" cellpadding="2" border="0">
+	<tr>
+		<td>Username</td>
+		<td><input type="text" name="username"></td>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td><input type="password" name="password"></td>
+	</tr>
+		<tr>
+		<td>&nbsp;</td>
 		<td><input type="submit" name="bttLogin" value="Login"></td>
 	</tr>
 	</table>
 </form>
 </div>
+
 </body>
 </html>
 
